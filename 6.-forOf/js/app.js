eeb3dc3e8498f4070147f44eda854039f721ae88
Liliwaukee <lilimmvisual@gmail.com>
2018-02-21 18:15:00 -0600

    agregando ejercicio 6.forOf

--- 6.-forOf/js/app.js
+++ 6.-forOf/js/app.js
@@ -1,33 +1,33 @@
                             // ES5
 
 // declara la variable
+/*
+var computeSumOfAllElements = function(array){
+  var totalSum = 0;
+  for(var i = 0; i < array.length; i++){
+    totalSum += array[i]
+  }
+  var average = totalSum / array.length;
+  return "El promedio de este arreglo es: " + average ;
+}
 
+var numbers = [10, 20, 30, 20, 30, 40, 60];
+console.log(computeSumOfAllElements(numbers));
+*/
 
-// escribe tu funcion
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                              //ES6
+//imprimir en consola el mensaje... "El promedio de este arreglo es: 30" "210"
+//ES6
 // Comenta todo lo anterior y escribelo en ES6
 
+const computeSumOfAllElements = array => {
 
-// declara la variable
 
+  let sumTotal = 0;
+  for (let value of array) {
+    sumTotal += value
+  }
+  return sumTotal/array.length
+}
 
-// escribe tu funcion
+var numbers = [10, 20, 30, 20, 30, 40, 60];
+console.log(computeSumOfAllElements(numbers));
